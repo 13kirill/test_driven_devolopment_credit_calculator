3c3aa89e2a9b24e9514c5af8d42a30b8671a007a
Kirill Zubkov <kirill131313@gmail.com>
2022-05-19 12:51:01 +0300

    Initial commit. Tests

---
+++ src/test/java/CreditCalculatorTest.java
@@ -0,0 +1,75 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CreditCalculatorTest {
+
+    CreditCalculator sut;
+
+    @BeforeEach
+    public void started() {
+        System.out.println("Test started");
+        sut = new CreditCalculator();
+    }
+
+    @BeforeAll
+    public static void startedAll() {
+        System.out.println("Tests started");
+    }
+
+    @AfterAll
+    public static void finishedAll() {
+        System.out.println("Tests completed");
+    }
+
+    @AfterEach
+    public void finished() {
+        System.out.println("Test completed");
+        sut = null;
+    }
+
+    @Test
+    public void monthlyPayment() {
+        //arrange
+        double percent = 8.2;
+        double credit = 3_411_300;
+        int months = 360;
+        double expexted = 25_508.15;
+
+        //act
+        double result = sut.monthlyPayment(credit, percent, months);
+
+        //assert
+        assertEquals(expexted, result);
+    }
+
+    @Test
+    public void returnedBalance() {
+        //arrange
+        double percent = 8.2;
+        double credit = 3_411_300;
+        int months = 360;
+        double expexted = 9182934.0;
+
+        //act
+        double result = sut.returnedBalance(credit, percent, months);
+
+        //assert
+        assertEquals(expexted, result);
+    }
+
+    @Test
+    public void overpayment() {
+        //arrange
+        double percent = 8.2;
+        double credit = 3_411_300;
+        int months = 360;
+        double expexted = 5771634.0;
+
+        //act
+        double result = sut.overpayment(credit, percent, months);
+
+        //assert
+        assertEquals(expexted, result);
+    }
+}
